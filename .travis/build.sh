29f781f1318b0b3183344e1fb243eed15a6cbac8
Vasar007 <vasar007@yandex.ru>
2018-10-14 21:24:50 +0300

    Fix SFML finding

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,7 +5,7 @@ set -x
 mkdir build
 mkdir build/TIMLE
 
-cmake -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION -DTARGET_CPU=$TARGET_CPU -DCMAKE_INSTALL_PREFIX=./build/TIMLE -DSFML_ROOT=./SFML/install . || exit 1
+cmake -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION -DTARGET_CPU=$TARGET_CPU -DCMAKE_INSTALL_PREFIX=./build/TIMLE -DCMAKE_PREFIX_PATH=./SFML/install . || exit 1
 make || exit 1
 make build || exit 1
 
