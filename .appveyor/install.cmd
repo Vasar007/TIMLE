059020d4d0a513c77817e3d160f3eaa80053b425
Vasar007 <vasar007@yandex.ru>
2018-10-15 15:11:05 +0300

    Fix mkdir command

--- .appveyor/install.cmd
+++ .appveyor/install.cmd
@@ -1,4 +1,3 @@
-echo "Start build"
 git clone https://github.com/SFML/SFML.git
 cd SFML
 
@@ -9,22 +8,15 @@ cd build
 cmake .. -G "Visual Studio 15 Win64" -DCMAKE_INSTALL_PREFIX=../install -DSFML_DEPENDENCIES_INSTALL_PREFIX=../install -DSFML_BUILD_FRAMEWORKS=FALSE -DBUILD_SHARED_LIBS=TRUE
 
 msbuild INSTALL.vcxproj
-echo "After SFML build"
 
 cd ../../
-echo "Process TIMLE project"
 cd TIMLE
-echo "cd into TIMLE project"
 
 mkdir build
-echo "mkdir build"
 
-echo "call ls"
-ls
-echo "end ls"
-
-mkdir build/TIMLE
+mkdir "build/TIMLE"
 echo "mkdir build/TIMLE"
 
+echo "Process TIMLE by CMake started"
 cmake . -G "Visual Studio 15 Win64" -DCMAKE_INSTALL_PREFIX=./build/TIMLE -DCMAKE_PREFIX_PATH=./SFML/install
-echo "Process TIMLE by CMake"
+echo "Process TIMLE by CMake ended"
