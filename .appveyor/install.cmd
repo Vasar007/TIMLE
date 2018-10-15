d8b98744082dc664ed324158933c7c8affc6d5e5
Vasar007 <vasar007@yandex.ru>
2018-10-15 19:08:59 +0300

    Fix position to current directory

--- .appveyor/install.cmd
+++ .appveyor/install.cmd
@@ -16,3 +16,5 @@ mkdir build
 cd build
 
 cmake .. -G "Visual Studio 15 Win64" -DCMAKE_BUILD_TYPE=$CONFIGURATION -DTARGET_CPU=$PLATFORM -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_PREFIX_PATH=C:/projects/timle/SFML/install
+
+cd ../
