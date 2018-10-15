91e7affde40923b9e33dde2e1d811aa7cc55b99c
Vasar007 <vasar007@yandex.ru>
2018-10-15 12:50:33 +0300

    Add echo messages

--- .appveyor/install.cmd
+++ .appveyor/install.cmd
@@ -1,3 +1,4 @@
+echo "Start build"
 git clone https://github.com/SFML/SFML.git
 cd SFML
 
@@ -8,12 +9,14 @@ cd build
 cmake .. -G "Visual Studio 15 Win64" -DCMAKE_INSTALL_PREFIX=../install -DSFML_DEPENDENCIES_INSTALL_PREFIX=../install -DSFML_BUILD_FRAMEWORKS=FALSE -DBUILD_SHARED_LIBS=TRUE
 
 msbuild INSTALL.vcxproj
+echo "After SFML build"
 
 cd ../../
 git clone https://github.com/Vasar007/TIMLE.git
+echo "After TIMPLE clonning"
 cd TIMLE
 
 mkdir build
 mkdir build/TIMLE
 
-cmake . -G "Visual Studio 15 Win64" -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION -DTARGET_CPU=$TARGET_CPU -DCMAKE_INSTALL_PREFIX=./build/TIMLE -DCMAKE_PREFIX_PATH=./SFML/install
+cmake . -G "Visual Studio 15 Win64" -DCMAKE_BUILD_TYPE=$CONFIGURATION -DTARGET_CPU=$PLATFORM -DCMAKE_INSTALL_PREFIX=./build/TIMLE -DCMAKE_PREFIX_PATH=./SFML/install
