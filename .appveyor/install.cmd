5079453ad2217ef5d76603eeb4a84685ac60c90a
Vasar007 <vasar007@yandex.ru>
2018-10-15 18:19:45 +0300

    Change SFML build path in the install script for manual check

--- .appveyor/install.cmd
+++ .appveyor/install.cmd
@@ -15,4 +15,4 @@ mkdir install
 mkdir build
 cd build
 
-cmake .. -G "Visual Studio 15 Win64" -DCMAKE_BUILD_TYPE=$CONFIGURATION -DTARGET_CPU=$PLATFORM -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_PREFIX_PATH=../SFML/install
+cmake .. -G "Visual Studio 15 Win64" -DCMAKE_BUILD_TYPE=$CONFIGURATION -DTARGET_CPU=$PLATFORM -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_PREFIX_PATH=C:/projects/timle/SFML/install
